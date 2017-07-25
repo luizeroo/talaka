2e4cbe34fe4fa4df6842f941361ee71aabda8cb0
José Luiz Reis <luizero.reis@gmail.com>
2017-07-25 20:02:08 -0300

    Grid, Layout, include JS and new mixins

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(main);
+
+function main() {
+    $(document).on('click', 'a', function (event) {
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 1000);
+    });
+}
