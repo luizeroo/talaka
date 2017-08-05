3533fa4d28b6789332aa3f7c91714c93cdaeaf32
José Luiz Reis <luizero.reis@gmail.com>
2017-08-05 15:25:29 -0300

    Sign in / Sign up pages

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 $(main);
 
 function main() {
-    $(document).on('click', 'a', function (event) {
-        event.preventDefault();
-
-        $('html, body').animate({
-            scrollTop: $($.attr(this, 'href')).offset().top
-        }, 1000);
-    });
+//    $(document).on('click', 'a[href="#howItWorks"', function (event) {
+//        event.preventDefault();
+//
+//        $('html, body').animate({
+//            scrollTop: $($.attr(this, 'href')).offset().top
+//        }, 1000);
+//    });
     
     $("#contrast").click(function(){
         $("body").css({"background-color":"#000", "color":"#fff"});
